2d64792e965bcf8844a3862f8d2913262ecc6419
Vitor Pantaleão <vitor-hmp@hotmail.com>
2022-02-03 23:58:34 -0300

    adiciona redes sociais

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,14 +1,14 @@
 import './menu.css'
-import { BsYoutube, BsInstagram } from 'react-icons/bs'
+import { BsInstagram, BsGithub } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
 
 export default function Menu(){
     return(
         <div className='menu'>
-            <a className='social' href='/#'>
-                <BsYoutube color='#FFF' size={24} />
+            <a className='social' href='https://github.com/vitorpantaleao/' target="blank">
+                <BsGithub color='#FFF' size={24} />
             </a>
-            <a className='social' href='/#'>
+            <a className='social' href='https://www.instagram.com/vitormassaro/' target="blank">
                 <BsInstagram color='#FFF' size={24} />
             </a>
             <Link className='menu-item' to="/links" >
